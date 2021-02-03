cd8d8f9e0e11752f0866460e39face9838c2067c
elodie <elodiebouthors@hotmail.com>
2021-02-03 05:40:10 +0000

    updates

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,12 +8,20 @@ for(var i = 0; i <= array1.length; i++) {
   }
 }
 return true;
-}
+};
+  //test comment
 
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 function assertArraysEqual(arr1, arr2){
   if (eqArrays (arr1, arr2)){
     console.log("yay")
   } else {
     console.log("nope")
   }
-}
+};
--- eqArrays.js
+++ eqArrays.js
@@ -15,7 +15,7 @@ assertEqual(1, 1);
 
 function eqArrays(array1, array2){
   if(array1.length !== array2.length) {
-    return "These are not the same length";
+    return false;
 }
 for(var i = 0; i <= array1.length; i++) {
   if(array1[i] !== array2[i]) {
