0fb6e1a4672b873b19290abc0acc5eb95b270e94
elodie <elodiebouthors@hotmail.com>
2021-02-04 17:32:06 +0000

    updates and new

--- eqObjects.js
+++ eqObjects.js
@@ -11,24 +11,40 @@ assertEqual("salut" , "Bootcamp");
 assertEqual(1, 1);
 
 
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+//worked with Elodie and Brody
 const eqObjects = function(object1, object2) {
+  const object1Keys = Object.keys(object1);
+  const object2Keys = Object.keys(object2);
+  console.log(object1Keys);
+  console.log(object2Keys);
+
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-  for (const key of Object.keys(object1)) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
+  for (let values of object1Keys) {
+    if (Array.isArray(object1Keys[values]) && Array.isArray(object2Keys[values])) {
+      if (!eqArrays(object1Keys, object2Keys)) {
         return false;
-      }
-    } else {
-      if (object1[key] !== object2[key]) {
-        return false;
+      } else {
+        if (object1Keys !== object2Keys) {
+          return false;
+        }
       }
     }
-  } 
+  }
   return true;
 };
-
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 eqObjects(ab, ba); // => true
@@ -37,17 +53,24 @@ const abc = { a: "1", b: "2", c: "3" };
 eqObjects(ab, abc); // => false
 
 
-//eqArrays function to check arrays:
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    for (let j = i; j <= i; j++) {
-      if (array1[i] !== array2[j]) {
-        return false;
-      }
+//code from Sam
+
+const eqObjects = function(object1, object2) {
+  let sortedFirstKeys = Object.keys(object1).sort(); // refer to the key of the object that was passed and sort it .
+  let sortedSecondKeys = Object.keys(object2).sort();
+
+  if (sortedFirstKeys.length !== sortedSecondKeys.length) { return false; }; // comparing length // technically we could delete this line, no need
+  if (!eqArrays(sortedFirstKeys, sortedSecondKeys)) { return false; }; // eq array compare arrays
+
+  for (let i = 0; i < sortedFirstKeys.length; i++) { //at this point we know they are same length and equal
+
+    if (Array.isArray(object1[sortedFirstKeys[i]]) && Array.isArray(object2[sortedSecondKeys[i]])) {
+      if (!eqArrays(object1[sortedFirstKeys[i]], object2[sortedSecondKeys[i]])) { return false; };
+    } else if (object1[sortedFirstKeys[i]] !== object2[sortedSecondKeys[i]]) { 
+      return false; // we comparing the value from obj 1 and 2 
     }
+
   }
+
   return true;
 };
--- letterPositions.js
+++ letterPositions.js
@@ -5,12 +5,15 @@
 const letterPositions = function(sentence) {
   const letterCount = {};
   
-  for (const index in sentence) {
-  const letter = sentence[index]
+  for (const index in sentence) { // for in loop same that the normal for loop (c style) !== for of loop give us the elements in the loop
+
+  const letter = sentence[index]  //[sentence comes from the in loop]
     if(letter !== " "){
       if(letterCount[letter]){
-        letterCount[letter].push(parseInt(index))
+        //if (letterCount ["l"]) // undifined because the key l doesn't exist yet // result is a falsy value
+        letterCount[letter].push(parseInt(index))  //parsInt will transfer from a string to a number
       } else {
+        // letterCount["l"]= 0
         letterCount[letter] = [parseInt(index)]
       }
     }
@@ -18,11 +21,14 @@ const letterPositions = function(sentence) {
   }
  
   return letterCount;
+  
 };
+
+// after firts loop : "l": [0]
 
 const result = letterPositions("lighthouse in the house")
 
-
+// Previsous function
 function assertArraysEqual(arr1, arr2){
   if (eqArrays (arr1, arr2)){
     console.log("yay")
@@ -31,7 +37,7 @@ function assertArraysEqual(arr1, arr2){
   }
 };
 
-
+//Previous function
 function eqArrays(array1, array2){
   if(array1.length !== array2.length) {
     return false;
@@ -45,3 +51,15 @@ return true;
 }
 
 assertArraysEqual (result["h"],[3,5,15,18])
+
+//other tests : 
+/* assertArraysEqual(testObj.l, [0]);
+assertArraysEqual(testObj.i, [1, 11]);
+assertArraysEqual(testObj.g, [2]);
+assertArraysEqual(testObj.h, [3, 5, 15, 18]);
+assertArraysEqual(testObj.t, [4, 14]);
+assertArraysEqual(testObj.o, [6, 19]);
+assertArraysEqual(testObj.u, [7, 20]);
+assertArraysEqual(testObj.s, [8, 21]);
+assertArraysEqual(testObj.e, [9, 16, 22]);
+assertArraysEqual(testObj.n, [12]);
