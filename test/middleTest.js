a330ccf8112244441641860e50693b24e0576f2c
elodie <elodiebouthors@hotmail.com>
2021-02-08 22:36:47 +0000

    updates refactor

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const eqArrays = require('../eqArrays');
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+//test
+middle([1, 2, 3]) // => [2]
+  middle([1, 2, 3, 4, 5]) // => [3]
+  middle([1, 2, 3, 4, 5, 6]) 
+
+  assertArraysEqual(middle([1]), []);
+  assertArraysEqual(middle([1, 2]), []);
+  assertArraysEqual(middle([1, 2, 3]), [2]);
